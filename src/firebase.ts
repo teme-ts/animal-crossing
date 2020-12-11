bcbd2e01f57f4189ba5af69f819da904e5729fb3
teme-ts <r1993socialwork@gmail.com>
2020-12-11 22:06:57 +0900

    firebaseの開発環境を作成

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,16 @@
-import firebaseConfig from './firebase_config'
 import firebase from 'firebase'
 import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_FIREBASE_CONFIG_APIKEY,
+    authDomain: process.env.VUE_APP_FIREBASE_CONFIG_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_CONFIG_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIREBASE_CONFIG_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_CONFIG_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_CONFIG_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_CONFIG_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_CONFIG_MEASUREMENT_ID
+  };
 
 firebase.initializeApp(firebaseConfig);
 
